0671b07d5f143bc36b5d173d80d230e150eb652e
Jorge Bush <jorge.bush12@gmail.com>
2024-11-06 14:50:17 -0500

    help me pull

--- client/src/api/movieApi.tsx
+++ client/src/api/movieApi.tsx
@@ -1,8 +1,6 @@
-// client/src/api/movieApi.ts
-
 import axios from 'axios';
 
-const API_KEY = process.env.REACT_APP_OMDB_API_KEY; 
+const API_KEY = process.env.REACT_APP_OMDB_API_KEY; // Use your API key from .env file
 
 // Function to fetch movie data from OMDb API
 export const fetchMovieData = async (imdbID: string) => {
