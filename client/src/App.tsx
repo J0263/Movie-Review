19c8020a81f88d408bac82317c20719347cc73d3
owengx <owengayle19@gmail.com>
2024-11-07 13:12:13 -0500

    install sequilize and pg

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import SearchPage from './pages/SearchPage';
-<<<<<<< HEAD
 // import WatchedPage from './pages/WatchedPage';
 // import ReviewPage from './pages/ReviewPage';
-import LoginPage from './pages/LoginPage';
-=======
-import ReviewPage from './pages/ReviewPage';
->>>>>>> 0671b07d5f143bc36b5d173d80d230e150eb652e
+// import LoginPage from './pages/LoginPage';
 import './App.css';
 // import LoginPage from './pages/LoginPage';
 // import WatchedPage from './pages/WatchedPage';
@@ -24,14 +20,8 @@ const App = () => {
             {/* <Route path="/" element={<HomePage />} /> */}
             <Route path="/search" element={<SearchPage />} />
             {/* <Route path="/your-watched" element={<WatchedPage />} /> */}
-<<<<<<< HEAD
             {/* <Route path="/write-review" element={<ReviewPage />} /> */}
-            <Route path="/login" element={<LoginPage />} />
-=======
-            <Route path="/write-review" element={<ReviewPage />} />
             {/* <Route path="/login" element={<LoginPage />} /> */}
-            <Route path="*" element={<div>Page not found</div>} /> {/* Fallback for unknown routes */}
->>>>>>> 0671b07d5f143bc36b5d173d80d230e150eb652e
           </Routes>
         </main>
         <Footer />
