d9a5bde477bdbee82bfaa9625a0f8814310952c2
Jorge Bush <jorge.bush12@gmail.com>
2024-11-07 12:30:08 -0500

    functioning search bar in header

--- client/src/pages/SearchPage.tsx
+++ client/src/pages/SearchPage.tsx
@@ -1,24 +1,27 @@
-import React from 'react'; 
-import { useLocation } from 'react-router-dom'; 
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 const SearchPage: React.FC = () => {
-  const location = useLocation(); // get current location from router
-  const { results } = location.state || { results: [] }; // extract results from location state or default to empty array
+    const location = useLocation();
+    const results = location.state?.results?.Search || []; // Access the results safely
 
-  return (
-    <div>
-      <h2>Search Results</h2> {/* display title for search results */}
-      {results && results.length > 0 ? ( // check if results exist and have length
-        <ul> 
-          {results.map((movie: any) => ( // iterate over results array
-            <li key={movie.imdbID}>{movie.Title}</li> // display movie title in list item
-          ))}
-        </ul> 
-      ) : (
-        <p>No results found.</p> 
-      )}
-    </div>
-  );
+    return (
+        <div>
+            <h1>Search Results</h1>
+            {results.length > 0 ? (
+                <ul>
+                    {results.map((movie: any) => (
+                        <li key={movie.imdbID}>
+                            <h2>{movie.Title}</h2>
+                            <img src={movie.Poster} alt={movie.Title} />
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>No movies found.</p>
+            )}
+        </div>
+    );
 };
 
-export default SearchPage; 
+export default SearchPage;
