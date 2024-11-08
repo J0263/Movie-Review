266f105361f79230c43640b210516ff572796640
Jorge Bush <jorge.bush12@gmail.com>
2024-11-08 13:17:51 -0500

    updated bg color

--- client/src/api/movieApi.tsx
+++ client/src/api/movieApi.tsx
@@ -4,6 +4,35 @@ import axios from 'axios';
 
 const API_KEY = import.meta.env.VITE_REACT_APP_OMDB_API_KEY; 
 
+// Define the structure of the movie data
+export interface MovieData {
+    Title: string;
+    Poster: string;
+    imdbID: string;
+    Year: string;
+    Rated: string;
+    Released: string;
+    Runtime: string;
+    Genre: string;
+    Director: string;
+    Actors: string;
+    Plot: string;
+    Language: string;
+    Country: string;
+    Awards: string;
+    Ratings: { Source: string; Value: string }[];
+    Metascore: string;
+    imdbRating: string;
+    imdbVotes: string;
+    Type: string;
+    DVD: string;
+    BoxOffice: string;
+    Production: string;
+    Website: string;
+    Response: string;
+    Error?: string;
+}
+
 // Function to fetch movie data from OMDb API by title
 export const fetchMovieData = async (title: string) => {
     try {
