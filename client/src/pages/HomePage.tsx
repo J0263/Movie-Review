d29d5ff3f44d9e120e22c79dfa081d1ef7e06bfd
Brandon <brandonlanes@gmail.com>
2024-11-08 11:46:55 -0500

    Updates to HomePage.tsx, now working as intended with random top movie generator from api database.

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
 
 
 const API_KEY = 'ec71b4f6';
